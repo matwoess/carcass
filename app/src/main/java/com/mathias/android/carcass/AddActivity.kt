2fda95e5476cb935874bb2f52d4980b868954b70
Mathias Wöß <mat.woess@gmail.com>
2019-11-28 01:27:31 +0100

    implemented Photo Capture with FileProvider in AddActivity

--- app/src/main/java/com/mathias/android/carcass/AddActivity.kt
+++ app/src/main/java/com/mathias/android/carcass/AddActivity.kt
@@ -1,28 +1,36 @@
 package com.mathias.android.carcass
 
+import android.content.Intent
+import android.graphics.BitmapFactory
 import android.location.Address
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.Spinner
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.material.snackbar.Snackbar
 import com.mathias.android.carcass.ActivityMaps.Companion.geocoder
+import java.io.File
+import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.min
 
 class AddActivity : AppCompatActivity() {
-    private lateinit var location: LatLng
-
     private lateinit var spnType: Spinner
     private lateinit var txtDescription: TextView
     private lateinit var txtTime: TextView
     private lateinit var txtLocation: TextView
     private lateinit var btnTakePicture: Button
     private lateinit var btnDone: Button
+    private lateinit var imageView: ImageView
+
+    private lateinit var location: LatLng
+
+    private var currentPhotoPath: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,17 +42,6 @@ class AddActivity : AppCompatActivity() {
         location = LatLng(lat, lng)
         initUI()
         initButtons()
-    }
-
-    private fun initButtons() {
-        btnTakePicture.setOnClickListener {
-            Snackbar.make(
-                findViewById(R.id.lyt_add_activity),
-                "Take picture",
-                Snackbar.LENGTH_SHORT
-            ).show()
-        }
-        btnDone.setOnClickListener { finish() }
     }
 
     private fun initUI() {
@@ -68,9 +65,75 @@ class AddActivity : AppCompatActivity() {
             1
         )
         txtLocation.text = if (addresses.isNotEmpty()) addresses[0].thoroughfare else "N/A"
+        imageView = findViewById(R.id.img_view_report)
+    }
+
+
+    private fun initButtons() {
+        btnTakePicture.setOnClickListener { dispatchTakePictureIntent() }
+        btnDone.setOnClickListener { finish() }
+    }
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    Log.e(TAG, "error creating image file")
+                    null
+                }
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "com.mathias.android.carcass",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.i(TAG, "picture taken")
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) setPic()
+    }
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_",
+            ".jpg",
+            storageDir
+        ).apply {
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    private fun setPic() {
+        val targetW: Int = imageView.width
+        val targetH: Int = imageView.height
+        val bmOptions = BitmapFactory.Options().apply {
+            inJustDecodeBounds = true
+            val photoW: Int = outWidth
+            val photoH: Int = outHeight
+            val scaleFactor: Int = min(photoW / targetW, photoH / targetH)
+            inJustDecodeBounds = false
+            inSampleSize = scaleFactor
+            inPurgeable = true
+        }
+        BitmapFactory.decodeFile(currentPhotoPath, bmOptions)?.also { bitmap ->
+            imageView.setImageBitmap(bitmap)
+        }
     }
 
     companion object {
         private const val TAG = "AddActivity"
+        private const val REQUEST_TAKE_PHOTO = 220
     }
 }
